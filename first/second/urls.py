5005d9071d5e4684f42d10572c5aed1ed7c2f0b4
AS-Aurora <aayushsharma.lucky97@gmail.com>
2025-05-10 23:05:38 +0530

    Using django template language

--- first/second/urls.py
+++ first/second/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from second import views
+from     second import views
 from django.contrib import admin
 
 urlpatterns = [
--- first/second/views.py
+++ first/second/views.py
@@ -11,13 +11,14 @@ def helloWorld(request):
 dummyData = [
     {
         'id': 1,
-        'name': 'John',
+        'name': 'john',
         'city': 'New York'
     },
     {
         'id': 2,
-        'name': 'Jane',
-        'city': 'Los Angeles'
+        'name': 'jane',
+        'city': 'Los Angeles',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
         'id': 3,
